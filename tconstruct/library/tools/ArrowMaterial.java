8f74af82723353084812177571ead6e65465b7a3
staplerstring <jbettenhausen436@gmail.com>
2016-03-07 11:46:36 -0600

    Added files via upload

---
+++ tconstruct/library/tools/ArrowMaterial.java
@@ -0,0 +1,22 @@
+package tconstruct.library.tools;
+
+public class ArrowMaterial
+{
+    public final float mass;
+    public final float breakChance;
+
+    public ArrowMaterial(float weight, float breakChance)
+    {
+        this.mass = weight;
+        this.breakChance = breakChance;
+    }
+
+    @Deprecated
+    public ArrowMaterial(float weight, float breakChance, float accuraccy)
+    {
+        this(weight, breakChance);
+    }
+
+    @Deprecated
+    public float accuracy = 0f; // tic-tooltips compatibility
+}
